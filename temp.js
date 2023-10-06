15f99fd4096088908482f5e9547135937f2526bc
Ljh <2169219371@qq.com>
2023-10-06 17:20:02 +0800

    我是第一版

---
+++ temp.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  new helloworld
+// @author       yutu
+// @match        https://blog.chrxw.com/cross.html
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=chrxw.com
+// @license         AGPL-3.0
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+      console.log("hello world");
+    // Your code here...
+})();
